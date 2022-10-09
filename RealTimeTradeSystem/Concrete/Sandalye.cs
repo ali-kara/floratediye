904e5e383f4203e1aad7346b6f2bf05f1f33b249
Ali Kara <alikara658@gmail.com>
2022-10-09 22:21:24 +0300

    LiveTrade added

---
+++ RealTimeTradeSystem/Concrete/Sandalye.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeEntities
+{
+    [Table("Sandalye")]
+    public class Sandalye
+    {
+        [Key]
+        public int Id { get; set; }
+        public int AliciCariKodu { get; set; }
+        
+        [StringLength(50)]
+        public string? AliciAdi { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsPressed { get; set; }
+        public decimal KalanKredi { get; set; }
+        public bool CheckActivate { get; set; }
+        public DateTime? LoginTime { get; set; }
+
+    }
+}
---
+++ RealTimeTradeSystem/RealTimeTradeSystem.DataAccess/RealTimeTradeDBContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeEntities
+{
+    public class LiveTradeDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=89.252.137.24;Database=CanliSatisDB;User ID=sa;Password=Yuskan2085@;MultipleActiveResultSets=true");
+            }
+        }
+
+        public DbSet<Alicilar> Alicilar { get; set; }
+        public DbSet<DepoGiris> DepoGiris { get; set; }
+        public DbSet<Sandalye> Sandalye { get; set; }
+        public DbSet<Satis> Satis { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Satis>().Property(e => e.ToplamTutar).HasPrecision(18, 2);
+            modelBuilder.Entity<Satis>().Property(e => e.DalFiyat).HasPrecision(18, 2);
+            modelBuilder.Entity<Satis>().Property(e => e.SatisFiyat).HasPrecision(18, 2);
+
+            modelBuilder.Entity<Sandalye>().Property(e => e.KalanKredi).HasPrecision(18, 2);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
